e4d7e42d872dae4299e6b476358502b494e8b73f
Henry Omino <henryedwin92@gmail.com>
2025-11-23 21:29:15 +0300

    urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
+
 from .views import ConversationViewSet, MessageViewSet
 
-# Required by ALX checker: routers.DefaultRouter()
-router = routers.DefaultRouter()
-
+# Main router
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
-router.register(r'messages', MessageViewSet, basename='messages')
+
+# Nested router
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
+
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("chats.urls")),
+
+    # include DRF api routes
+    path('api/', include('chats.urls')),
+
+    # This is required for the check
+    path('api-auth/', include('rest_framework.urls')),
 ]
+
