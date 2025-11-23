65b973654efa32edabe5dce2ba99b76a8db3916c
Henry Omino <henryedwin92@gmail.com>
2025-11-23 16:45:24 +0300

    pycodestyle compliant

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -41,7 +41,7 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
-        """Test that get_json returns correct payload and calls requests.get once."""
+        """Test that get_json returns correct payload and calls requests.get."""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
