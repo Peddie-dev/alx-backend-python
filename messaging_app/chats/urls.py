52b1d15f5ebbf7de4b2cc3dc81158908335d049d
Henry Omino <henryedwin92@gmail.com>
2025-11-23 21:24:42 +0300

    urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-# ALX checker requirement: routers.DefaultRouter()
+# Required by ALX checker: routers.DefaultRouter()
 router = routers.DefaultRouter()
-router.register(r"conversations", ConversationViewSet, basename="conversations")
-router.register(r"messages", MessageViewSet, basename="messages")
+
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path('', include(router.urls)),
 ]
